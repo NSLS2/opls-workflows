76cb832f2ce5c3c1530ac8d676f5150734b65e86
Juan Marulanda Arias <jmaruland@bnl.gov>
2025-07-15 10:25:56 -0400

    Modified code to allow concurrent tasks

--- data_validation.py
+++ data_validation.py
@@ -1,7 +1,7 @@
 import os
 import time as ttime
 
-from prefect import flow, task
+from prefect import flow, task, get_run_logger
 from tiled.client import from_profile
 
 BEAMLINE_OR_ENDSTATION = "opls"
@@ -9,21 +9,22 @@ BEAMLINE_OR_ENDSTATION = "opls"
 
 @task(retries=2, retry_delay_seconds=10)
 def read_all_streams(uid, beamline_acronym=BEAMLINE_OR_ENDSTATION):
+    logger = get_run_logger()
     tiled_client = from_profile("nsls2")
     run = tiled_client[beamline_acronym]["raw"][uid]
-    print(f"Validating uid {run.metadata['start']['uid']}")
+    logger.info(f"Validating uid {run.start['uid']}")
     start_time = ttime.monotonic()
     for stream in run:
-        print(f"{stream}:")
+        logger.info(f"{stream}:")
         stream_start_time = ttime.monotonic()
         stream_data = run[stream].read()
         stream_elapsed_time = ttime.monotonic() - stream_start_time
-        print(f"{stream} elapsed_time = {stream_elapsed_time}")
-        print(f"{stream} nbytes = {stream_data.nbytes:_}")
+        logger.info(f"{stream} elapsed_time = {stream_elapsed_time}")
+        logger.info(f"{stream} nbytes = {stream_data.nbytes:_}")
     elapsed_time = ttime.monotonic() - start_time
-    print(f"{elapsed_time = }")
+    logger.info(f"{elapsed_time = }")
 
 
 @flow(log_prints=True)
 def data_validation(uid):
-    read_all_streams(uid)
+    read_all_streams(uid, beamline_acronym="opls")
