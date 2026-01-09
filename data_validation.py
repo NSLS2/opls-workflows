a0616892ffc3cf2734c4df62136e8442a79301b2
Jun Aishima <jaishima@bnl.gov>
2026-01-09 15:46:30 -0500

    fix name of Tiled API key block

--- data_validation.py
+++ data_validation.py
@@ -10,7 +10,7 @@ BEAMLINE_OR_ENDSTATION = "opls"
 @task(retries=2, retry_delay_seconds=10)
 def read_all_streams(uid, beamline_acronym=BEAMLINE_OR_ENDSTATION):
     logger = get_run_logger()
-    api_key = Secret.load(f"tiled.{beamline_acronym}.api.key", _sync=True).get()
+    api_key = Secret.load(f"tiled-{beamline_acronym}-api-key", _sync=True).get()
     tiled_client = from_profile(beamline_acronym, api_key=api_key)[beamline_acronym]
     run = tiled_client["raw"][uid]
     logger.info(f"Validating uid {run.start['uid']}")
