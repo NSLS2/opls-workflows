3f163b6bf0c75c2e9e8ea66f2c827a5fb9150411
Jun Aishima <jaishima@bnl.gov>
2026-05-01 17:04:31 -0400

    change to dotenv, extract client to get_run()

--- data_validation.py
+++ data_validation.py
@@ -1,18 +1,32 @@
 import time as ttime
+import os
 
 from prefect import flow, task, get_run_logger
-from prefect.blocks.system import Secret
-from tiled.client import from_profile
+from tiled.client import from_uri
 
 BEAMLINE_OR_ENDSTATION = "opls"
+
+
+def get_api_key_from_env():
+    with open("/srv/container.secret", "r") as secrets:
+        load_dotenv(stream=secrets)
+    api_key = os.environ["TILED_API_KEY"]
+    return api_key
+
+
+@task(retries=2, retry_delay_seconds=10)
+def get_run(uid, api_key=None):
+    if not api_key:
+        api_key = get_api_key_from_env()
+    cl = from_uri("https://tiled.nsls2.bnl.gov", api_key=api_key)
+    run = cl[f"{BEAMLINE_OR_ENDSTATION}/raw"][uid]
+    return run
 
 
 @task(retries=2, retry_delay_seconds=10)
 def read_all_streams(uid, beamline_acronym=BEAMLINE_OR_ENDSTATION):
     logger = get_run_logger()
-    api_key = Secret.load(f"tiled-{beamline_acronym}-api-key", _sync=True).get()
-    tiled_client = from_profile("nsls2", api_key=api_key)[beamline_acronym]
-    run = tiled_client["raw"][uid]
+    run = get_run(uid)
     logger.info(f"Validating uid {run.start['uid']}")
     start_time = ttime.monotonic()
     for stream in run:
